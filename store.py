cbed3eac76c1976523c84ae611d7a15136202d43
MADjnr <maduafamdaniel@gmail.com>
2022-03-19 10:34:53 +0100

    added store

---
+++ store.py
@@ -0,0 +1,25 @@
+class Store:
+
+    def __init__(self, name, address, num_employees):
+        self.name = name
+        self.address = address
+        self.num_employees = num_employees
+
+    def greet_customers(self):
+        print("Welcome to our store! What would you like to buy?")
+
+
+class DonutStore(Store):
+
+    def __init__(self, name, address, num_employees):
+        Store.__init__(self, name, address, num_employees)
+
+    def greet_customers(self):
+        print("welcome to our store what donut would you like to eat")
+
+
+my_donut_store = DonutStore('round donut', 'Onuiyi', 24)
+
+print(my_donut_store.name)
+print(my_donut_store.address)
+
